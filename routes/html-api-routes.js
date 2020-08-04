90ef84fb02b74f5c33b96e8336132a3f74246729
lathisha <lathisha.n@gmail.com>
2020-08-04 11:23:08 -0700

    added code from vacation: api routes to get/put

--- routes/html-api-routes.js
+++ routes/html-api-routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-const db = require("../models/Workout");
+const Workout = require("../models/Workout");
 
 // ----------HTML Routes---------- 
 router.get("/", (res) => {
@@ -18,3 +18,31 @@ router.get("/stats", (req, res) => {
 });
 
 // ----------API Routes---------- 
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    let duration = + req.body.duration
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises: req.body,
+        }, totalDuration: duration
+    }).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+        .catch(({ message }) => {
+            console.log(message);
+        });
+});
+
+
+
+module.exports = router;
